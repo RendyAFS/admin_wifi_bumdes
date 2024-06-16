3fdde41bdf86d5908a25fbd7c93e58557cdc632e
RendyAFS <arendy724@gmail.com>
2024-06-17 01:01:22 +0700

    initial build

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+    <title>Bumdes Gondo Bening</title>
+    @vite('resources/sass/app.scss')
+</head>
+
+<body>
+    <div class="container m-5">
+       <a href="" class="btn btn-success">
+            Tes
+       </a>
+    </div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
