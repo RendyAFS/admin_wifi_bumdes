0610611140528e1867b537bdf74409285a2bc9af
Ralfhstr <ralfh.str@gmail.com>
2024-06-25 22:13:07 +0700

    feat(landing page): 20%

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a href="/">
+                <img src="{{ Vite::asset('public/images/assets/logo-bumdes-t.png') }}" alt="Logo" id="logo-navbar">
+            </a>
+            <button class="navbar-toggler border border-0" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="bi bi-list fs-3"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fs-6" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-6" href="">Harga Layanan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fs-6" href="">Testimoni</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Langganan
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#"></a>-</li>
+                        </ul>
+                      </li>
+
+                </ul>
+                <a class="icon-link icon-link-hover link-offset-2 link-underline link-underline-opacity-0 text-dark fw-bold fs-6"
+                    href="/login">
+                    Log In
+                    <i class="bi bi-box-arrow-in-right fs-5 d-flex"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
