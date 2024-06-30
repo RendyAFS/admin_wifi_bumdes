3960eb362bfbdd534913fcdb27ae2baee1e25406
Ralfhstr <ralfh.str@gmail.com>
2024-06-30 15:26:54 +0700

    feat(landing page): 80%

---
+++ resources/views/informasi.blade.php
@@ -0,0 +1,172 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="mb-5">
+        <div class="container">
+            
+        </div>
+    </section>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L60,122.7C120,149,240,203,360,224C480,245,600,235,720,208C840,181,960,139,1080,122.7C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+        </path>
+    </svg>
+    <section class="gradient-bg">
+        <div class="container">
+            <h2 class="text-center p">Berita Layanan</h2>
+            <hr class="hr-light">
+            <p class="text-center p">Media Layanan Internet.</p>
+            <div class="row justify-content-center align-items-center">
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card m-2" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L80,90.7C160,85,320,75,480,96C640,117,800,171,960,170.7C1120,171,1280,117,1360,90.7L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+        </path>
+    </svg>
+
+    <table class="table">
+        <thead class="table-light">
+            ...
+        </thead>
+        <tbody>
+            ...
+        </tbody>
+    </table>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,62 +1,187 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-none d-md-flex justify-content-center align-items-center home">
-        <div class="">
-            <img src="{{ Vite::asset('public/images/assets/logo-iconnet-t.png') }}" alt="Logo" id="logo-iconnet">
-        </div>
-        <div class="mt-5">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui vulputate,
-                vehicula nunc eu, eleifend neque. Sed ac sagittis leo. Aenean arcu orci, fermentum nec commodo ut, pharetra
-                ullamcorper felis. Vivamus id magna in urna laoreet efficitur. Nulla tristique auctor auctor. In molestie
-                vel nunc finibus bibendum.
-            </p>
-        </div>
-    </div>
-    <div class="page-with-svg-background">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#192857" fill-opacity="1"
-                d="M0,192L80,186.7C160,181,320,171,480,181.3C640,192,800,224,960,218.7C1120,213,1280,171,1360,149.3L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
-            </path>
-        </svg>
-        <svg style="margin-top: -1px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#192857" fill-opacity="1" d="M0,64L0,160L1440,160L1440,0L0,0L0,0Z"></path>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#192857" fill-opacity="1"
-                d="M0,192L80,186.7C160,181,320,171,480,181.3C640,192,800,224,960,218.7C1120,213,1280,171,1360,149.3L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
-            </path>
-        </svg>
-        <div class="content">
+    <section id="Home">
+        <div class="d-none d-md-flex justify-content-center align-items-center home">
+            <div class="">
+                <img src="{{ Vite::asset('public/images/assets/logo-iconnet-t.png') }}" alt="Logo" id="logo-iconnet">
+            </div>
             <div class="mt-5">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui vulputate,
-                    vehicula nunc eu, eleifend neque. Sed ac sagittis leo. Aenean arcu orci, fermentum nec commodo ut,
-                    pharetra
-                    ullamcorper felis. Vivamus id magna in urna laoreet efficitur. Nulla tristique auctor auctor. In
-                    molestie
-                    vel nunc finibus bibendum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui vulputate,
-                    vehicula nunc eu, eleifend neque. Sed ac sagittis leo. Aenean arcu orci, fermentum nec commodo ut,
-                    pharetra
-                    ullamcorper felis. Vivamus id magna in urna laoreet efficitur. Nulla tristique auctor auctor. In
-                    molestie
-                    vel nunc finibus bibendum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui vulputate,
-                    vehicula nunc eu, eleifend neque. Sed ac sagittis leo. Aenean arcu orci, fermentum nec commodo ut,
-                    pharetra
-                    ullamcorper felis. Vivamus id magna in urna laoreet efficitur. Nulla tristique auctor auctor. In
-                    molestie
-                    vel nunc finibus bibendum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui vulputate,
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper nibh. Aenean eu dui
+                    vulputate,
                     vehicula nunc eu, eleifend neque. Sed ac sagittis leo. Aenean arcu orci, fermentum nec commodo ut,
                     pharetra
                     ullamcorper felis. Vivamus id magna in urna laoreet efficitur. Nulla tristique auctor auctor. In
                     molestie
                     vel nunc finibus bibendum.
                 </p>
+                <div style="text-align: center">
+                    <button class="btn btn-primary ">Langganan Sekarang</button>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
+
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L60,122.7C120,149,240,203,360,224C480,245,600,235,720,208C840,181,960,139,1080,122.7C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+        </path>
+    </svg>
+    <section id="Layanan" class="gradient-bg">
+        <div class="container">
+            <h2 class="text-center p">Layanan</h2>
+            <hr class="hr-light">
+            <p class="text-center p">Sesuaikan Bandwidth Sesuai Kebutuhan Anda</p>
+            <div class="row justify-content-center">
+                <div class="card-bg row">
+                    <div class="col-md-3">
+                        <div class="card-bnwth">
+                            <div class="card-body text-center">
+                                <div class="bnwth">
+                                    <h1 class="b">20</h1>
+                                    <h6>Mbps</h6>
+                                </div>
+                                <h5>Fiber Home</h5>
+                                <p>Up to <span style="color: #0B8EAE;">20 Mbps</span> Unlimited Quota Download / Upload</p>
+                                <p><b>Gratis Biaya Pasang</b></p>
+                                <button class="btn btn-primary">Hubungi Sekarang</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card-bnwth">
+                            <div class="card-body text-center">
+                                <div class="bnwth">
+                                    <h1 class="b">35</h1>
+                                    <h6>Mbps</h6>
+                                </div>
+                                <h5>Fiber Home</h5>
+                                <p>Up to <span style="color: #0B8EAE;">35 Mbps</span> Unlimited Quota Download / Upload</p>
+                                <p><b>Gratis Biaya Pasang</b></p>
+                                <button class="btn btn-primary">Hubungi Sekarang</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card-bnwth">
+                            <div class="card-body text-center">
+                                <div class="bnwth">
+                                    <h1 class="b">50</h1>
+                                    <h6>Mbps</h6>
+                                </div>
+                                <h5>Fiber Home</h5>
+                                <p>Up to <span style="color: #0B8EAE;">50 Mbps</span> Unlimited Quota Download / Upload</p>
+                                <p><b>Gratis Biaya Pasang</b></p>
+                                <button class="btn btn-primary">Hubungi Sekarang</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card-bnwth">
+                            <div class="card-body text-center">
+                                <div class="bnwth">
+                                    <h1 class="b">100</h1>
+                                    <h6>Mbps</h6>
+                                </div>
+                                <h5>Fiber Home</h5>
+                                <p>Up to <span style="color: #0B8EAE;">100 Mbps</span> Unlimited Quota Download / Upload</p>
+                                <p><b>Gratis Biaya Pasang</b></p>
+                                <button class="btn btn-primary">Hubungi Sekarang</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L80,90.7C160,85,320,75,480,96C640,117,800,171,960,170.7C1120,171,1280,117,1360,90.7L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+        </path>
+    </svg>
+
+    <section>
+        <div id="Testimoni" class="container">
+            <h2 class="text-center">Testimoni</h2>
+            <hr class="hr-blue">
+            <p class="text-center">Kata Mereka yang Sudah Memakai</p>
+            <div class="row">
+                <div class="card-group">
+                    <div class="card">
+                        <img src="{{ Vite::asset('public/images/assets/testi.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">JOSSS</h5>
+                            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper
+                                nibh..</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="{{ Vite::asset('public/images/assets/testi.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">MANTAPP</h5>
+                            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper
+                                nibh..</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="{{ Vite::asset('public/images/assets/testi.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">YOMANN</h5>
+                            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu semper
+                                nibh..</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L60,122.7C120,149,240,203,360,224C480,245,600,235,720,208C840,181,960,139,1080,122.7C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+        </path>
+    </svg>
+    <section id="Pelayanan" class="gradient-bg">
+        <div class="container">
+            <h2 class="text-center p">Pelayanan</h2>
+            <hr class="hr-light">
+            <p class="text-center p">Dokumentasi layanan yang diberikan</p>
+            <div class="row justify-content-center">
+                <div class="card-plynn row">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <defs>
+            <linearGradient id="gradient1" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" style="stop-color:#192857;" />
+                <stop offset="100%" style="stop-color:#0B8EAE;" />
+            </linearGradient>
+        </defs>
+        <path fill="url(#gradient1)" fill-opacity="1"
+            d="M0,96L80,90.7C160,85,320,75,480,96C640,117,800,171,960,170.7C1120,171,1280,117,1360,90.7L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
+        </path>
+    </svg>
 @endsection
